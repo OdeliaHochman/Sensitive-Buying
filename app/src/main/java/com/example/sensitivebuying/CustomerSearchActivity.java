812d04aa368ff4563647d591a55645085fe18e11
caron1211 <caron1211@gmail.com>
2019-11-25 20:58:13 +0200

    empty activites

---
+++ app/src/main/java/com/example/sensitivebuying/CustomerSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.sensitivebuying;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_search);
+    }
+}
---
+++ app/src/main/java/com/example/sensitivebuying/UpdateSensitivesActivity.java
@@ -0,0 +1,14 @@
+package com.example.sensitivebuying;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateSensitivesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_sensitives);
+    }
+}
