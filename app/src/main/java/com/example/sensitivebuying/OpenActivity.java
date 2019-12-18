b419e20cfb7b2cf5f7669d780ebf044098dd99ea
caron1211 <caron1211@gmail.com>
2019-12-18 20:41:02 +0200

    check login in open

--- app/src/main/java/com/example/sensitivebuying/OpenActivity.java
+++ app/src/main/java/com/example/sensitivebuying/OpenActivity.java
@@ -4,17 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class OpenActivity extends AppCompatActivity {
 
     final String activity = " OpenActivity";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser autoUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("debug",activity);
+        firebaseAuth = FirebaseAuth.getInstance();
+        autoUser = firebaseAuth.getCurrentUser();
         setContentView(R.layout.activity_open);
+
+        if(autoUser!=null ) {
+            setUser();
+        }
 
         new Thread(new Runnable() {
             @Override
@@ -33,5 +51,34 @@ public class OpenActivity extends AppCompatActivity {
                 }
             }
         }).start();
+
+    }
+
+
+    private void setUser()
+    {
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference uidRef = rootRef.child("Users").child(uid);
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("rep").getValue(Boolean.class).equals(true)) {
+                    startActivity(new Intent(OpenActivity.this, RepresentativeHomeActivity.class));
+                    finish(); return;
+                } else {
+                    startActivity(new Intent(OpenActivity.this, HostNavigationActivity.class));
+                    finish(); return;
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("firebase", databaseError.getMessage());
+            }
+        };
+        uidRef.addListenerForSingleValueEvent(valueEventListener);
+
     }
 }
